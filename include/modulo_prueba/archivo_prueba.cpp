6b04e467b8cf959d0a09316b6de78989e0610749
Banzi Alvarez <ianbanzialvarezf@gmail.com>
2025-10-03 20:32:57 -0600

    commit inicial

---
+++ include/modulo_prueba/archivo_prueba.cpp
@@ -0,0 +1,9 @@
+#include "archivo_prueba.h"
+
+archivo_prueba::archivo_prueba(/* args */)
+{
+}
+
+int archivo_prueba::sumaPrueba(int numero){
+    return numero++;
+}
---
+++ include/modulo_prueba/archivo_prueba.h
@@ -0,0 +1,16 @@
+#ifndef ARCHIVO_PRUEBA_H
+#define ARCHIVO_PRUEBA_H
+
+class archivo_prueba
+{
+private:
+    /* data */
+public:
+    archivo_prueba(/* args */);
+    int sumaPrueba(int numero);
+   
+};
+
+
+
+#endif
